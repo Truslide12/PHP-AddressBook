63a1c3289078fa5d851552a0aaffaaf6bee8d680
Carlos Aragon <carlos.s.aragon12@gmail.com>
2020-01-22 22:19:28 -0800

    initial sort of contacts complete. Paginate complete. Trying to figure out how to sort the contacts by clicking on each header

--- addressbook/app/Http/Controllers/ContactController.php
+++ addressbook/app/Http/Controllers/ContactController.php
@@ -16,7 +16,7 @@ class ContactController extends Controller
      */
     public function index()
     {
-        $contacts = Contact::all();//->paginate(5);
+        $contacts = DB::table('contacts')->orderBy('firstName')->simplePaginate(10);
         return view('contacts.index', ['contacts' => $contacts]);
               // ->with("i", (request()->input('page',1) -1) *5);
     }
@@ -146,10 +146,89 @@ class ContactController extends Controller
         return view('contacts.createAddress')->with('contact_id', $contact_id);
     }
 
-    // public function sort($value)
-    // {
-    //     $contacts = Contact::all();
-    //     $sorted = $contacts->sortBy($value);
-    //     return view('contacts.index', ['contacts' => $sorted]);
-    // }
+    public function sort( Request $request)
+    {
+        dump($request);
+        $column = $request->column;
+        $contacts = DB::table('contacts')->orderBy($column)->simplePaginate(10);
+        return view('contacts.sort', ['contacts' => $contacts]);
+        
+        // $contacts = DB::table('contacts')->sortBy($id);
+        // return view('contacts.index', compact($contacts));
+
+// th = document.getElementsByTagName('th');
+// console.log(th);
+// console.log(th.length);
+// for (let c = 0; c < (th.length - 1); c++) {
+//     th[c].addEventListener('click', item(c))
+// }
+
+// function item(c) {
+//     return function () {
+//         sortTable(c)
+//     }
+// }
+
+// function sortTable(c) {
+//     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+//     table = document.getElementById("contactsTable");
+//     switching = true;
+//     // Set the sorting direction to ascending:
+//     dir = "asc";
+//     /* Make a loop that will continue until
+//     no switching has been done: */
+//     while (switching) {
+//         // Start by saying: no switching is done:
+//         switching = false;
+//         rows = table.rows;
+//         /* Loop through all table rows (except the
+//         first, which contains table headers): */
+//         for (i = 1; i < (rows.length - 1); i++) {
+//             // Start by saying there should be no switching:
+//             shouldSwitch = false;
+//             /* Get the two elements you want to compare,
+//             one from current row and one from the next: */
+//             x = rows[i].getElementsByTagName("TD")[c];
+//             y = rows[i + 1].getElementsByTagName("TD")[c];
+//             /* Check if the two rows should switch place,
+//             based on the direction, asc or desc: */
+//             if (dir == "asc") {
+//                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+//                     // If so, mark as a switch and break the loop:
+//                     shouldSwitch = true;
+//                     break;
+//                 }
+//             } else if (dir == "desc") {
+//                 if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+//                     // If so, mark as a switch and break the loop:
+//                     shouldSwitch = true;
+//                     break;
+//                 }
+//             }
+//         }
+//         if (shouldSwitch) {
+//             /* If a switch has been marked, make the switch
+//             and mark that a switch has been done: */
+//             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//             switching = true;
+//             // Each time a switch is done, increase this count by 1:
+//             switchcount++;
+//         } else {
+//             /* If no switching has been done AND the direction is "asc",
+//             set the direction to "desc" and run the while loop again. */
+//             if (switchcount == 0 && dir == "asc") {
+//                 dir = "desc";
+//                 switching = true;
+//             }
+//         }
+//     }
+// }
+
+    }
+
+    public function sortDesc($id)
+    {
+        $contacts = DB::table('contacts')->sortByDesc($id);
+        return view('contacts.index', compact($contacts));
+    }
 }
